34b7757e4cf8df04b3f4018e05d522eaa1bd5c98
chrisbodhi <cboette@gmail.com>
2015-08-09 22:24:27 +0000

    Add styled header component. Tie controller to header. Add function to toggle links for signing in and signing out.

--- app/components/header/header-directive.js
+++ app/components/header/header-directive.js
@@ -3,10 +3,19 @@
 angular.module('myApp.header', [])
 .controller('HeaderController', ['$scope', function($scope) {
   $scope.signedIn = false;
+  
+  $scope.logIn = function(){
+    $scope.signedIn = !$scope.signedIn;
+  }
+  
+  $scope.logOut = function(){
+    $scope.signedIn = !$scope.signedIn;
+  }
 }])
 .directive('appHeader', function() {
   return {
     restrict: 'E',
-    templateUrl: 'components/header/header.html'
+    templateUrl: 'components/header/header.html',
+    controller: 'HeaderController'
   };
 });
