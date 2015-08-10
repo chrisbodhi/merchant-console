cfb5ca74a8ec88a1a6dd903585d2ca66b98aaaa5
chrisbodhi <cboette@gmail.com>
2015-08-10 18:04:45 +0000

    Add placeholder ping page/

--- app/ping/ping.js
+++ app/ping/ping.js
@@ -9,6 +9,12 @@ angular.module('myApp.ping', ['ngRoute'])
   });
 }])
 
-.controller('PingController', [function() {
-
+.controller('PingController', ['$scope', function($scope) {
+  $scope.pings = [
+    {event: "Bourbon special", end: "8/12/15"},
+    {event:"Welcome the new barback", end: "8/22/15"},
+    {event:"Yer favorite band", end: "8/22/15"},
+    {event:"Quarter moon", end: "8/22/15"},
+    {event:"Book release party", end: "8/22/15"}
+  ];
 }]);
